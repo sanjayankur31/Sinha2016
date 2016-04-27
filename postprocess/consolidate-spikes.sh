bd02dc8d2b13e3f723ec06664990373d0204ef3f
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2016-04-27 10:59:03 +0100

    Add consolidate script and extend firing rate plotter

---
+++ postprocess/consolidate-spikes.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Copyright 2015 Ankur Sinha 
+# Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com> 
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# File : consolidate-spikes.sh
+#
+
+echo "Combining files"
+mkdir consolidated_files
+
+(
+echo "Combining recall files"
+sort -k "2" -n --parallel=16 spikes-*recall*.gdf > spikes-recall.gdf
+mv spikes-recall.gdf consolidated_files
+)
+(
+echo "Combining pattern files"
+sort -k "2" -n --parallel=16 spikes-*pattern*.gdf > spikes-pattern.gdf
+mv spikes-pattern.gdf consolidated_files
+)
+(
+echo "Combining E files"
+sort -k "2" -n --parallel=16 spikes-*E*.gdf > spikes-E.gdf
+mv spikes-E.gdf consolidated_files
+)
+(
+echo "Combining I files"
+sort -k "2" -n --parallel=16 spikes-*I*.gdf > spikes-I.gdf
+mv spikes-I.gdf consolidated_files
+)
--- postprocess/plot-firing-rates.plt
+++ postprocess/plot-firing-rates.plt
@@ -8,6 +8,9 @@ set output "firing-rate-E.png"
 set title "Firing rate for E neurons"
 plot "firing-rate-E.gdf" with lines lw 4 title "";
 
+set output "firing-rate-pattern.png"
+set title "Firing rate for pattern and recall neurons"
+plot "firing-rate-pattern.gdf" with lines lw 4 title "P", "firing-rate-recall.gdf" with lines lw 4 title "R";
 
 set output "firing-rate-I.png"
 set title "Firing rate for I neurons"
@@ -16,3 +19,7 @@ plot "firing-rate-I.gdf" with lines lw 4 title "";
 set output "firing-rate-both.png"
 set title "Firing rate for neurons"
 plot "firing-rate-I.gdf" with lines lw 4 title "I", "firing-rate-E.gdf" with lines lw 4 title "E";
+
+set output "firing-rate-all.png"
+set title "Firing rate for various neuron sets"
+plot "firing-rate-I.gdf" with lines lw 4 title "I", "firing-rate-E.gdf" with lines lw 4 title "E" , "firing-rate-pattern.gdf" with lines lw 4 title "P", "firing-rate-recall.gdf" with lines lw 4 title "R";
