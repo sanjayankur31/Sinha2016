89d51311042af255a716a7dc7d17ed84c6a4eb2a
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2016-05-10 14:10:45 +0100

    Update cluster script

--- scripts/cluster/nest-runsim.sh
+++ scripts/cluster/nest-runsim.sh
@@ -25,18 +25,33 @@
 #PBS -N "nest-v-s"
 
 module unload mpi/mpich-x86_64
-module load openmpi
+module load mvapich2-1.7
 
 SOURCE_PATH="/home/asinha/Documents/02_Code/00_repos/00_mine/Sinha2016/src/Sinha2016.py"
 RESULT_PATH="/stri-data/asinha/results/"
 SIM_TIME=$(date +%Y%m%d%H%M)
+
+echo ------------------------------------------------------
+echo -n 'Job is running on node '; cat $PBS_NODEFILE
+echo ------------------------------------------------------
+echo PBS: qsub is running on $PBS_O_HOST
+echo PBS: originating queue is $PBS_O_QUEUE
+echo PBS: executing queue is $PBS_QUEUE
+echo PBS: working directory is $PBS_O_WORKDIR
+echo PBS: execution mode is $PBS_ENVIRONMENT
+echo PBS: job identifier is $PBS_JOBID
+echo PBS: job name is $PBS_JOBNAME
+echo PBS: node file is $PBS_NODEFILE
+echo PBS: current home directory is $PBS_O_HOME
+echo PBS: PATH = $PBS_O_PATH
+echo ------------------------------------------------------
 
 echo "ANKUR>> Begun at $SIM_TIME"
 
 mkdir $RESULT_PATH/$SIM_TIME
 cd $RESULT_PATH/$SIM_TIME
 
-mpirun -n 40 python $SOURCE_PATH
+/usr/local/bin/mpiexec -n 40 python $SOURCE_PATH
 
 END_TIME=$(date +%Y%m%d%H%M)
 echo "ANKUR>> Ended at $END_TIME"
