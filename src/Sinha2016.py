0a2d21ee5645430d346a328bb5ba551b225a8a61
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2017-06-11 23:52:44 +0100

    Make network deaff independent of pattern storage

--- src/Sinha2016.py
+++ src/Sinha2016.py
@@ -1928,19 +1928,20 @@ if __name__ == "__main__":
         # only track first pattern to limit log files
         simulation.store_lpz_central_pattern(True)
         simulation.store_lpz_border_pattern(True)
-
         # stabilise network after storing patterns
         simulation.stabilise()
 
-        # Deaff first pattern (which is also being tracked)
-        simulation.deaff_network()
-        # Enable structural plasticity for repair #
-        simulation.enable_rewiring()
-        # Stabilise for repair
-        simulation.stabilise()
-        simulation.stabilise()
+    # Deaff network
+    simulation.deaff_network()
+    # Enable structural plasticity for repair #
+    simulation.enable_rewiring()
+    # Stabilise for repair
+    simulation.stabilise()
+    simulation.stabilise()
 
+    if store_patterns:
         # recall stored and tracked pattern
+        # TODO: REDO recall
         simulation.recall_pattern(50, 1)
 
     simulation.close_files()
