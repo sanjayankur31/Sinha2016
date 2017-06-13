1f132adede24500c21682ecb5e7cd2ac9bb055c0
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2017-06-13 11:28:53 +0100

    Use smaller lpz percent - 75 is too big

--- src/Sinha2016.py
+++ src/Sinha2016.py
@@ -1981,7 +1981,7 @@ if __name__ == "__main__":
     # update of the network
     simulation.setup_plasticity(True, True)
     # set up deaff extent, and neuron sets
-    simulation.set_lpz_percent(0.75)
+    simulation.set_lpz_percent(0.3)
     # set up neurons, connections, spike detectors, files
     simulation.prerun_setup(
         stabilisation_time=2000.,
