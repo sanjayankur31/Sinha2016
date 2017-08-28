96987362ea81ad7149f4f75e9bf7b4a0a00d4b26
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2017-08-28 17:42:43 +0100

    Increase stabilisation time

--- src/Sinha2016.py
+++ src/Sinha2016.py
@@ -2163,7 +2163,7 @@ if __name__ == "__main__":
     # update time windows
     # we update connectivity every 2 seconds, and dump data every 50 seconds
     # in the paper, they updated connectivity every 100ms
-    simulation.update_time_windows(stabilisation_time=10000.,
+    simulation.update_time_windows(stabilisation_time=15000.,
                                    sp_update_interval=2.,
                                    recording_interval=50.)
     # Stabilise for repair
