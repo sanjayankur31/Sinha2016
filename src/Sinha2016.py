ada64c762813cad07ea099d39c750b7e671b140a
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2018-03-21 13:34:59 +0000

    Disable zoomed in phase

--- src/Sinha2016.py
+++ src/Sinha2016.py
@@ -2921,10 +2921,12 @@ if __name__ == "__main__":
         # Deaff network
         simulation.deaff_network()
         # zoom in on post deaff phase
+        """
         simulation.update_time_windows(stabilisation_time=50.,
                                        sp_update_interval=1.,
                                        recording_interval=5.)
         simulation.stabilise(label="Repair zoomed in")
+        """
 
         simulation.update_time_windows(stabilisation_time=4000.,
                                        sp_update_interval=1.,
