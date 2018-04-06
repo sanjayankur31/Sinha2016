aab7df0540b0bc3ef5e8e28aef0681762d0afeef
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2018-04-06 15:19:01 +0100

    Shift axonal growth curve

--- src/Sinha2016.py
+++ src/Sinha2016.py
@@ -949,18 +949,18 @@ class Sinha2016:
         # For E
         mean_ca_e = numpy.mean(ca_e)
         mean_ca_i = numpy.mean(ca_i)
-        self.eps_ax_e = mean_ca_e * 2.5
+        self.eps_ax_e = mean_ca_e * 1.1
         self.eps_den_e_e = mean_ca_e
         self.eps_den_e_i = mean_ca_e * 3.0
-        self.eta_ax_e = mean_ca_e
+        self.eta_ax_e = mean_ca_e * 0.9
         self.eta_den_e_e = mean_ca_e * 0.10
         self.eta_den_e_i = mean_ca_e
 
         # For I
-        self.eps_ax_i = mean_ca_i * 2.5
+        self.eps_ax_i = mean_ca_i * 1.1
         self.eps_den_i_e = mean_ca_i
         self.eps_den_i_i = mean_ca_i * 3.0
-        self.eta_ax_i = mean_ca_i
+        self.eta_ax_i = mean_ca_i * 0.9
         self.eta_den_i_e = mean_ca_i * 0.10
         self.eta_den_i_i = mean_ca_i
 
