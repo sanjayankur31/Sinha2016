77e839a4e5efc0aadc9c57c0cea2876f42b1cf31
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2018-06-11 19:36:52 +0100

    Reduce simulation times

--- src/Sinha2016.py
+++ src/Sinha2016.py
@@ -3200,7 +3200,7 @@ if __name__ == "__main__":
 
     #  Stabilise with both plasticities active
     #  update time windows
-    simulation.update_time_windows(stabilisation_time=2500.,
+    simulation.update_time_windows(stabilisation_time=1500.,
                                    sp_update_interval=1.,
                                    recording_interval=100.)
     simulation.stabilise(label="Syn and str together")
@@ -3216,7 +3216,7 @@ if __name__ == "__main__":
         simulation.stabilise(label="Repair zoomed in")
         """
 
-        simulation.update_time_windows(stabilisation_time=10000.,
+        simulation.update_time_windows(stabilisation_time=5000.,
                                        sp_update_interval=1.,
                                        recording_interval=100.)
         simulation.stabilise(label="Repair #2")
