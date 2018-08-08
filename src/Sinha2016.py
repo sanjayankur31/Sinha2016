fef56a824ee9c1b05dc261ba66ff86594876ce25
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2018-08-08 13:05:24 +0100

    Increase stabilisation times to permit network to settle down completely at each stage

--- src/Sinha2016.py
+++ src/Sinha2016.py
@@ -3065,7 +3065,7 @@ if __name__ == "__main__":
     # set up deaff extent, and neuron sets
     simulation.set_lpz_percent(0.1)
     # set up neurons, connections, spike detectors, files
-    simulation.update_time_windows(stabilisation_time=1500.,
+    simulation.update_time_windows(stabilisation_time=3000.,
                                    sp_update_interval=1.,
                                    recording_interval=100.)
     simulation.setup_simulation()
@@ -3096,7 +3096,7 @@ if __name__ == "__main__":
 
     #  Stabilise with both plasticities active
     #  update time windows
-    simulation.update_time_windows(stabilisation_time=1500.,
+    simulation.update_time_windows(stabilisation_time=2000.,
                                    sp_update_interval=1.,
                                    recording_interval=100.)
     simulation.stabilise()
@@ -3112,7 +3112,7 @@ if __name__ == "__main__":
         simulation.stabilise(label="Repair zoomed in")
         """
 
-        simulation.update_time_windows(stabilisation_time=5000.,
+        simulation.update_time_windows(stabilisation_time=10000.,
                                        sp_update_interval=1.,
                                        recording_interval=100.)
         simulation.stabilise(label="Repair #2")
