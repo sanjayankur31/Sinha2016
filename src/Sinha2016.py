801b27ff413e916949d7dda2c6a02e4388174009
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2019-01-03 14:48:24 +0000

    Further tweaks to growth curves

--- src/Sinha2016.py
+++ src/Sinha2016.py
@@ -118,25 +118,28 @@ class Sinha2016:
         # maximum value of dz/dt per time step
         # z += dz
         # base value
-        self.nu = 1e-5
+        self.nu = 5e-5
         # value for excitatory neurons
         self.nu_e = self.nu
         self.nu_den_e_e = 1 * self.nu_e
         # inhibitory elements react faster than excitatory elements
         # mostly no spines, so quicker
-        self.nu_den_e_i = 10 * self.nu_e
+        self.nu_den_e_i = 2 * self.nu_e
         # 8000 excitatory neurons provide input to 10000 neurons of the
         # network, so they must be slightly faster than the post-synaptic
         # elements
         self.nu_ax_e = 10 * self.nu
 
         # post-synaptic elements of inhibitory neurons react slower to activity
-        # changes than excitatory neurons
-        self.nu_i = self.nu * 0.1
+        # changes than excitatory neurons so that the inhibitory network
+        # turns over quicker and prevents the excitatory network from modifying
+        # itself too much: assumption being that the excitatory is the
+        # functional aspect and therefore should not change too much.
+        self.nu_i = self.nu * 0.5
         self.nu_den_i_e = 1 * self.nu_i
         # inhibitory elements react faster than excitatory elements
         # mostly no spines, so quicker
-        self.nu_den_i_i = 10 * self.nu_i
+        self.nu_den_i_i = 2 * self.nu_i
         # 2000 inhibitory neurons inhibit 10000 neurons of the network, so the
         # inhibitory axon must act at a faster rate
         self.nu_ax_i = 10 * self.nu
