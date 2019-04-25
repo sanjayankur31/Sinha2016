dfc7847b954b05dd5406beb5ac6a5717404c1137
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2019-04-25 12:01:59 +0100

    Move axonal curves up on Y to have similar retraction rates for ax and den

--- src/Sinha2016.py
+++ src/Sinha2016.py
@@ -149,11 +149,11 @@ class Sinha2016:
         self.tau_den_i_e = 0.01
         self.tau_den_i_i = 0.01
 
-        self.omega_ax_e = 0.2
+        self.omega_ax_e = 0.05
         self.omega_den_e_e = 0.4
         self.omega_den_e_i = 0.4
 
-        self.omega_ax_i = 0.2
+        self.omega_ax_i = 0.025
         self.omega_den_i_e = 0.4
         self.omega_den_i_i = 0.4
 
